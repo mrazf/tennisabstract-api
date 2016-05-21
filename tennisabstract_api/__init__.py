a0f31d781b6167eb24d6157957afaa7c674c4e04
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2016-05-21 15:01:02 +0100

    Messing around with elasticache

--- tennisabstract_api/__init__.py
+++ tennisabstract_api/__init__.py
@@ -1,20 +1,38 @@
 from flask import Flask, jsonify
 from selenium import webdriver
+import redis
 
 app = application = Flask(__name__)
+cache = redis.StrictRedis(host='tennisabstract-cache.9lg61v.0001.euw1.cache.amazonaws.com', port=6379)
 driver = webdriver.PhantomJS('./node_modules/phantomjs/bin/phantomjs')
 
 @app.route("/api/player/<name>")
 def player(name):
+    result = get_from_cache(name)
+    print result
+    if result: return result
+
     driver.get("http://www.tennisabstract.com/cgi-bin/player.cgi?p=" + name)
     biography = driver.find_element_by_id("biog")
     player_name = biography.find_element_by_xpath("./table/tbody/tr[1]/td/span").text
     player_dob = biography.find_element_by_xpath("./table/tbody/tr[2]/td").text
 
-    return jsonify({
-            "name": player_name,
-            "dateOfBirth": player_dob
-        })
+    result = jsonify({
+        "name": player_name,
+        "dateOfBirth": player_dob
+    })
+    store_in_cache(name, result)
+
+    return result
+
+
+def get_from_cache(name):
+    return cache.get(name)
+
+
+def store_in_cache(key, val):
+    cache.set(key, val)
+
 
 @app.after_request
 def add_header(response):
